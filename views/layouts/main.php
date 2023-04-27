99a76bd413c0a4ce2371d63170e595d0f741894d
Rodelio <rodeliobdomingojr@gmail.com>
2023-04-28 00:23:11 +0800

    remove the my application

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,7 +32,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <header id="header">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        // 'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
     ]);
